706ca7d737e6a3b0c8261d67127fbb3211c33a69
justbennet <justbennet@users.noreply.github.com>
2017-02-19 08:52:28 -0500

    Update bet_test.py

--- BasicPython/bet_test.py
+++ BasicPython/bet_test.py
@@ -16,7 +16,8 @@ subject = 'sub' + str(id).zfill(3)
 top_dir = os.getcwd()
 os.chdir(subject)
 # Do work
-subprocess.check_output(['bet', 'struct.nii.gz', 'struct_bet.nii.gz', '-v'])
+bet_output = subprocess.check_output(['bet', 'struct.nii.gz', 'struct_bet.nii.gz', '-v'])
+print(bet_output)
 
 # Return to top
 os.chdir(top_dir)
