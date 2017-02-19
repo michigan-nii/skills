3aa5a7c435612ed9b19276b8d99520c73e90c463
justbennet <justbennet@users.noreply.github.com>
2017-02-19 09:00:51 -0500

    Update bet_test_nipype.py

--- BasicPython/bet_test_nipype.py
+++ BasicPython/bet_test_nipype.py
@@ -6,16 +6,18 @@ my_home = os.path.expanduser('~')
 data_dir = os.path.join(my_home, 'nipype_tutorial', 'data')
 os.chdir(data_dir)
 
-for id in range(1,11):
-    subject = 'sub' + str(id).zfill(3)
-    top_dir = os.getcwd()
-    os.chdir(subject)
-    mybet = fsl.BET(in_file='struct.nii.gz',
-                    out_file='struct_bet.nii.gz')
-    mybet.run()
-    os.chdir(top_dir)
+id = 1
 
-###  Note, you may prefer these other methods to the one above.
+subject = 'sub' + str(id).zfill(3)
+top_dir = os.getcwd()
+os.chdir(subject)
+mybet = fsl.BET(in_file='struct.nii.gz',
+                out_file='struct_bet.nii.gz')
+mybet.run()
+os.chdir(top_dir)
+
+###  Note, there are other ways to fill in the needed information,
+###  some of which you might find more readable
 ###  Examples from
 #    http://miykael.github.io/nipype-beginner-s-guide/firstSteps.html
 
