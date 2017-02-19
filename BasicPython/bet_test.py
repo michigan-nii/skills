18551fcfcc426ddf915eb4d7dde3b82d64c4cc5b
justbennet <justbennet@users.noreply.github.com>
2017-02-19 08:51:05 -0500

    Update bet_test.py

--- BasicPython/bet_test.py
+++ BasicPython/bet_test.py
@@ -1,15 +1,22 @@
 # Script to test that bet is callable from Python
 import subprocess
 import os
-import gzip
 
 my_home = os.path.expanduser('~')
 data_dir = os.path.join(my_home, 'nipype_tutorial', 'data')
 os.chdir(data_dir)
 
-for id in range(1,11):
-    subject = 'sub' + str(id).zfill(3)
-    top_dir = os.getcwd()
-    os.chdir(subject)
-    subprocess.check_output(['bet', 'struct.nii.gz', 'struct_bet.nii.gz'])
-    os.chdir(top_dir)
+# Set ID number
+id = 1
+
+# Format subject directory string
+subject = 'sub' + str(id).zfill(3)
+
+# Save where we are, then go to subject folder
+top_dir = os.getcwd()
+os.chdir(subject)
+# Do work
+subprocess.check_output(['bet', 'struct.nii.gz', 'struct_bet.nii.gz', '-v'])
+
+# Return to top
+os.chdir(top_dir)
